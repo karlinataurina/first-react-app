26d5ac2c18b6f044bbfa7f9e0872e650564cb2e1
karlinataurina <karlina.taurina@gmail.com>
2023-01-19 07:27:49 +0200

    .

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,10 +1,10 @@
 import ExpenseItem from './ExpenseItem';
+import Card from './Card';
 import './Expenses.css';
 
 function Expenses(props) {
     return (
-        <div className="expenses">
-            <div className="expenses">
+        <Card className="expenses">
             <ExpenseItem
                 title={props.items[0].title}
                 amount={props.items[0].amount}
@@ -21,8 +21,7 @@ function Expenses(props) {
                 title={props.items[3].title}
                 amount={props.items[3].amount}
                 date={props.items[3].date} />
-            </div>
-        </div>
+        </Card>
     );
 }
 
